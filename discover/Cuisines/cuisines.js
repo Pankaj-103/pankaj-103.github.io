139d052075c31dec6f1104d5fc859d5750cae167
pankaj-103 <pankaj.cs20@bmsce.ac.in>
2022-07-30 00:51:55 +0530

    my 3rd sem project

---
+++ discover/Cuisines/cuisines.js
@@ -0,0 +1,34 @@
+
+let location=['Visveswarapura,Basvangudi',
+'Lavella road,Ashok nagar',
+'Koramangla','10th A Main road,Jayanagar',
+'Gandhi bazaar,Basvangudi',
+'Residency road',
+'Ground floor raheja arcade,Koramangla',
+'Whitefield'];
+
+let description=['very nice place',
+'love this place',
+'fond of eating',
+'i want to live here',
+' marvellous place',
+'i got goosebumps ',
+'lovely adorable',
+'nice thing to be done here'];
+
+let images=['https://i.ytimg.com/vi/UxNDW6qIsgY/maxresdefault.jpg',
+'https://i.ytimg.com/vi/Aauzq0OEtII/maxresdefault.jpg',
+'https://b.zmtcdn.com/data/pictures/1/50691/92d9b4053ef0965120828b4fa4eecc3b.jpg',
+'https://content3.jdmagicbox.com/comp/bangalore/n1/080pxx80.xx80.170413090736.v9n1/catalogue/kapoor-s-cafe-hsr-layout-bangalore-north-indian-restaurants-6i1q67rr6m.jpg',
+'https://i0.wp.com/bangalorelocale.com/wp-content/uploads/2021/04/Vidyarthi-Bhavan-Bangalore.jpg?fit=900%2C506&ssl=1',
+'https://lh5.googleusercontent.com/p/AF1QipMtYXardcM0Vt5OUflL6nU65q6yPFCmQnqrgJV1=w1080-k-no',
+'https://imgmedia.lbb.in/media/2019/10/5dadcd4f48dddf262b92c0c0_1571671375382.jpg',
+'https://media-cdn.tripadvisor.com/media/photo-s/0e/52/4b/45/img-20170131-152223-burst1.jpg',
+];
+
+
+
+function show(){
+    let name=document.getElementById('name').innerHTML;
+    console.log(name);
+}
